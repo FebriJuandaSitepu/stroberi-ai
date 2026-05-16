d16fc2fcb0099244398be9a62f4f46e854cb9206
FebriJuandaSitepu <febrijuanda250204@gmail.com>
2026-05-16 17:26:55 +0700

    update

--- lib/models/history_model.dart
+++ lib/models/history_model.dart
@@ -1,63 +1,39 @@
-
+import 'dart:convert';
 import 'dart:typed_data';
 
 class HistoryModel {
 
   final String result;
-
   final double confidence;
-
   final String imagePath;
-
   final DateTime createdAt;
-
   final Uint8List? imageBytes;
 
   HistoryModel({
-
     required this.result,
-
     required this.confidence,
-
     required this.imagePath,
-
     required this.createdAt,
-
     this.imageBytes,
   });
 
   Map<String, dynamic> toJson() {
-
     return {
-
       'result': result,
-
       'confidence': confidence,
-
       'imagePath': imagePath,
-
-      'createdAt':
-          createdAt.toIso8601String(),
+      'createdAt': createdAt.toIso8601String(),
+      'imageBytes': imageBytes != null ? base64Encode(imageBytes!) : null, // <-- tambah ini
     };
   }
 
-  factory HistoryModel.fromJson(
-    Map<String, dynamic> json,
-  ) {
-
+  factory HistoryModel.fromJson(Map<String, dynamic> json) {
     return HistoryModel(
-
       result: json['result'],
-
-      confidence:
-          json['confidence'],
-
-      imagePath:
-          json['imagePath'],
-
-      createdAt: DateTime.parse(
-        json['createdAt'],
-      ),
+      confidence: json['confidence'],
+      imagePath: json['imagePath'],
+      createdAt: DateTime.parse(json['createdAt']),
+      imageBytes: json['imageBytes'] != null ? base64Decode(json['imageBytes']) : null, // <-- tambah ini
     );
   }
 }
